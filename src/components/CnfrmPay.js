917ed0a343119c54e5d91e6eb7d03499b92a9bf6
dVshar <101096861+dVshar@users.noreply.github.com>
2023-10-17 15:23:55 +0530

    updated

--- src/components/CnfrmPay.js
+++ src/components/CnfrmPay.js
@@ -62,8 +62,8 @@ function CnfrmPay(props) {
     const [PaymentStatus, setPaymentStatus] = useState("PENDING")
     const [PayStatus, setPayStatus] = useState("PAID")
     
-    const baseUrl = "https://nexon.eazotel.com"
-    // const baseUrl = "http://127.0.0.1:5000"
+    // const baseUrl = "https://nexon.eazotel.com"
+    const baseUrl = "http://127.0.0.1:5000"
     // location api
 
     useEffect(() => {
@@ -144,6 +144,7 @@ function CnfrmPay(props) {
         const json = await response.json();
 
         if (json.Status === true) {
+            console.log(json)
             props.setPayment({
                 "Status": true,
                 "Order": json.order_id,  // Order ID from the payment gateway
