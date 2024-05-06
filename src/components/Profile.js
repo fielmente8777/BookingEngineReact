9238b860942c3391ee1dfa59bef906fdc97825a6
abhijeet <abhijeet@eazotel.com>
2024-05-06 13:30:40 +0530

    up

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import "../style/Profile.css"
+import { FaUserLarge } from "react-icons/fa6";
 const Profile = () => {
     return (
-        <div>Profile</div>
+        <div className='profile_container'>
+            <div className='procontent'>
+                <div className='profileicon'>
+                    <FaUserLarge color='white' size={100} />
+                </div>
+                <div className='mt-3'>9645789243</div>
+
+            </div>
+            <div className='fo'>
+                <input type='text' value={""} name='firstname' />
+                <input type='text' value={""} name='lastname' />
+                <input type='email' value={""} name='email' />
+                <div className='butoon'>
+                    <button>SAVE</button>
+                    <button>RESET</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
